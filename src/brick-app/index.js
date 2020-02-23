d9a6812559c09f3e8e23c9f6669eafef7d32edc7
Patrick Gasseels <grasseelsp@hotmail.com>
2020-02-23 13:56:49 +0100

    Init

---
+++ src/brick-app/index.js
@@ -0,0 +1,37 @@
+const brickpi3 = require('brickpi3');
+const signalR = require('@microsoft/signalr');
+const HOST = process.env.HOST || 'http://localhost:5000';
+const CLIENTNAME = process.env.CLIENTNAME || 'defautlClient';
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl(`${HOST}/brickpi`)
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+let BP = new brickpi3.BrickPi3();
+brickpi3.utils.resetAllWhenFinished(BP);
+
+function registerClient() {
+    connection.send('RegisterClient', CLIENTNAME)
+        .then(() => {
+            startListenning();
+        });
+}
+
+function startListenning() {
+
+}
+
+(async () => {
+    try {
+        connection.start().then(function () {
+            console.log("Connected");
+            registerClient();
+        });
+
+    } catch (err) {
+        console.log(err);
+    }
+})();
+
+
